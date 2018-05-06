e72d8fc9bae3bdc832ca3dd9236c8bf1d2a410fc
Vijay <vijaygunaraj@gmail.com>
2018-05-06 10:12:42 +0530

    Initial Commit 06 - 05 -2018

--- app/src/main/java/com/mobile/framework/anti_theft/Common/IBaseClassInterface.java
+++ app/src/main/java/com/mobile/framework/anti_theft/Common/IBaseClassInterface.java
@@ -2,7 +2,5 @@ package com.mobile.framework.anti_theft.Common;
 
 public interface IBaseClassInterface {
 
-    public void ReturnResult(Object result);
-
-
+     void ReturnResult(Object result);
 }
